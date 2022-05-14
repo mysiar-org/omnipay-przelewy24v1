fb7c1e395881bb5d01164b360a30e8b60f3a3fd4
Piotr Synowiec <psynowiec@gmail.com>
2022-05-14 13:50:23 +0200

    transaction-verify: in progress

---
+++ src/Message/CompletePurchaseRequest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omnipay\Przelewy24\Message;
+
+use Omnipay\Common\Message\ResponseInterface;
+
+class CompletePurchaseRequest extends AbstractRequest
+{
+    public function getSessionId(): string
+    {
+        return $this->getParameter('sessionId');
+    }
+
+    public function setSessionId(string $value): self
+    {
+        return $this->setParameter('sessionId', $value);
+    }
+
+    public function getAmount(): int
+    {
+        return $this->getParameter('amount');
+    }
+
+    public function setAmount($value): self
+    {
+        return $this->setParameter('amount', $value);
+    }
+
+    public function getData()
+    {
+        $this->validate('sessionId', 'amount', 'currency', 'transactionId');
+
+        return [
+            'merchantId' => $this->getMerchantId(),
+            'posId' => $this->getPosId(),
+            'sessionId' => $this->getSessionId(),
+            'amount' => $this->getAmount(),
+            'currency' => $this->getCurrency(),
+            'orderId' => $this->getTransactionId(),
+            'sign' => $this->generateSignature(),
+        ];
+    }
+
+    public function sendData($data)
+    {
+        $httpResponse = $this->sendRequest('PUT', 'transaction/verify', $data);
+
+        $responseData = json_decode($httpResponse->getBody()->getContents(), true);
+
+        return $this->response = new CompletePurchaseResponse($this, $responseData, $this->getEndpoint());
+    }
+
+    private function generateSignature(): string
+    {
+        return hash(
+            self::SIGN_ALGO,
+            json_encode(
+                [
+                    'sessionId' => $this->getSessionId(),
+                    'orderId' => (int) $this->getTransactionId(),
+                    'amount' => (int) $this->getAmount(),
+                    'currency' => $this->getCurrency(),
+                    'crc' => $this->getCrc(),
+                ],
+                JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
+            )
+        );
+    }
+}
--- tests-api/README.md
+++ tests-api/README.md
@@ -2,6 +2,7 @@
 
 **Requirement**
 
+* https://omnipay-przelewy24v1.requestcatcher.com if not exists create
 * set your params in .env
 * run `source .env`
 
