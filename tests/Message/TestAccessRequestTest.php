fb17f0abba02b3620a9b3abc2a7ea9e8df944335
Piotr Synowiec <psynowiec@gmail.com>
2022-05-20 16:36:55 +0200

    additional-tests: TestAccess tests

---
+++ tests/Message/TestAccessRequestTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Message;
+
+use Omnipay\Przelewy24\Message\TestAccessRequest;
+use Omnipay\Przelewy24\Message\TestAccessResponse;
+use Omnipay\Tests\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestAccessRequestTest extends TestCase
+{
+    /**
+     * @var TestAccessRequest
+     */
+    private $request;
+
+    public function setUp()
+    {
+        $this->request = new TestAccessRequest($this->getHttpClient(), $this->getHttpRequest());
+        $this->request->initialize([]);
+    }
+
+    public function testGetData(): void
+    {
+        $data = $this->request->getData();
+
+        $this->assertSame([], $data);
+    }
+
+    public function testSendSuccess(): void
+    {
+        $this->setMockHttpResponse('TestAccessSuccess.txt');
+        $response = $this->request->send();
+
+        $this->assertInstanceOf(TestAccessResponse::class, $response);
+        $this->assertTrue($response->isSuccessful());
+        $this->assertSame(Response::HTTP_OK, $response->getCode());
+        $this->assertSame('', $response->getMessage());
+    }
+
+    public function testSendAuthFailure()
+    {
+        $this->setMockHttpResponse('TestAccessAuthFailure.txt');
+        $response = $this->request->send();
+
+        $this->assertInstanceOf(TestAccessResponse::class, $response);
+        $this->assertFalse($response->isSuccessful());
+        $this->assertSame(Response::HTTP_UNAUTHORIZED, $response->getCode());
+        $this->assertSame('Incorrect authentication', $response->getMessage());
+    }
+
+    public function testSendInvalidDataFailure()
+    {
+        $this->setMockHttpResponse('TestAccessInvalidDataFailure.txt');
+        $response = $this->request->send();
+
+        $this->assertInstanceOf(TestAccessResponse::class, $response);
+        $this->assertFalse($response->isSuccessful());
+        $this->assertSame(Response::HTTP_BAD_REQUEST, $response->getCode());
+        $this->assertSame('Invalid input data', $response->getMessage());
+    }
+}
